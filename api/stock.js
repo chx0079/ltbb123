6cb369f282eb5e50e4f3cacd7eb7cfbed7630944
陈浩熙 <chenhaoxi@chenhaoxideMacBook-Air.local>
2026-02-20 10:58:50 +0800

    Fix: Add CORS headers and improve Vercel API config

--- api/stock.js
+++ api/stock.js
@@ -1,6 +1,14 @@
 const https = require('https');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   try {
     const { path } = req.query;
 
