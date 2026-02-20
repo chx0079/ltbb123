379296c150b2237725a3650170783be73faab524
陈浩熙 <chenhaoxi@chenhaoxideMacBook-Air.local>
2026-02-20 11:24:35 +0800

    Fix: Convert api/stock.js to ES Module syntax

--- api/stock.js
+++ api/stock.js
@@ -1,6 +1,6 @@
-const https = require('https');
+import https from 'https';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
